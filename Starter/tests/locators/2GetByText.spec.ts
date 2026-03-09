702b6e6d42d36007df2a6037cce441b5f3372b7b
marcio <mvcf09@gmail.com>
2026-03-09 13:29:56 -0300

    commit modulo 18, getByText, error messages and hidden features

--- Starter/tests/locators/2GetByText.spec.ts
+++ Starter/tests/locators/2GetByText.spec.ts
@@ -1,43 +1,35 @@
 import { test, expect } from '@playwright/test';
 
-test('Get by text practice', async ({ page }) => {
+test('18 - Get by text', async ({ page }) => {
     await page.goto('FeedBackForm.html')
 
     const title = page.getByText('Feedback Form').first();
     await expect(title).toBeVisible()
 })
 
-test('Get by text practice - error messages', async ({ page }) => {
+test('18 - Get by text - error messages', async ({ page }) => {
     await page.goto('FeedBackForm.html')
-    
+
     const emailValidationMessage = page.getByText('Invalid email format')
     await expect(emailValidationMessage).toBeHidden()
 
     await page.getByRole('textbox', {
-        name: 'email'
-    }).fill('john@email')
+        name:'email' 
+    }).fill('jjasdij@gmail')
     await expect(emailValidationMessage).toBeVisible()
 
     await page.getByRole('textbox', {
-        name: 'email'
-    }).fill('john@email.com')
-    await expect(emailValidationMessage).toBeHidden()
+        name:'email' 
+    }).fill('jjasdij@gmail.com')
+    await expect(emailValidationMessage).not.toBeVisible()
 })
 
-
-test('Get by text practice - hidden elements', async ({ page }) => {
+test('18 - Get by text - hidden features', async ({ page }) => {
     await page.goto('FeedBackForm.html')
-    
-    const hiddenButton = page.getByText('Hidden feature') 
-    await expect(hiddenButton).toBeHidden() 
+
+    const hiddenButton = page.getByText('Hidden feature')
+    await expect(hiddenButton).not.toBeVisible()
 
     const hiddenButtonText = await hiddenButton.textContent()
     console.log(hiddenButtonText)
-
-    const hiddenButtonWithRole = page.getByRole('button', {
-        name: 'Hidden feature'
-    }) 
-
-    // not working
-    // const hiddenButtonWithRoleText = await hiddenButtonWithRole.textContent()
 })
